58bde3954d9f18de07b467e6a789e7928098d285
Pot4too <alexbaris167@gmail.com>
2026-06-28 21:07:45 +0200

    Test main file to see if board and setup works correctly

---
+++ firmware/src/main.c
@@ -0,0 +1,20 @@
+// src/main.c
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_PIN GPIO_NUM_15
+
+void app_main(void)
+{
+    gpio_reset_pin(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        gpio_set_level(LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
